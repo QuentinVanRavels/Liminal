9ae9b91a277261498fe6e9d050c1b1690cbcbdfb
QuentinVR <quentin.vanravels@telenet.be>
2020-08-15 18:16:12 +0200

    Initial commit

---
+++ Source/Liminal/LiminalGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "LiminalGameMode.h"
+#include "LiminalHUD.h"
+#include "LiminalCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ALiminalGameMode::ALiminalGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ALiminalHUD::StaticClass();
+}
---
+++ Source/Liminal/LiminalHUD.h
@@ -0,0 +1,25 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "LiminalHUD.generated.h"
+
+UCLASS()
+class ALiminalHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	ALiminalHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
