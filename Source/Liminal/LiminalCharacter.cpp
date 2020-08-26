2383e270efae897a0296cc36efb53471e65f2819
ElieVanRavels <elie.vanravels@telenet.be>
2020-08-26 12:08:30 +0200

    recommit

--- Source/Liminal/LiminalCharacter.cpp
+++ Source/Liminal/LiminalCharacter.cpp
@@ -11,6 +11,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "MotionControllerComponent.h"
 #include "XRMotionControllerBase.h" // for FXRMotionControllerBase::RightHandSourceId
+#include "UObject/ConstructorHelpers.h"
 #include "DrawDebugHelpers.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
@@ -33,51 +34,9 @@ ALiminalCharacter::ALiminalCharacter()
 	FirstPersonCameraComponent->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f)); // Position the camera
 	FirstPersonCameraComponent->bUsePawnControlRotation = true;
 
-	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
-	//Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
-	//Mesh1P->SetOnlyOwnerSee(true);
-	//Mesh1P->SetupAttachment(FirstPersonCameraComponent);
-	//Mesh1P->bCastDynamicShadow = false;
-	//Mesh1P->CastShadow = false;
-	//Mesh1P->SetRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
-	//Mesh1P->SetRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
-
-	//// Create a gun mesh component
-	//FP_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
-	//FP_Gun->SetOnlyOwnerSee(true);			// only the owning player will see this mesh
-	//FP_Gun->bCastDynamicShadow = false;
-	//FP_Gun->CastShadow = false;
-	//// FP_Gun->SetupAttachment(Mesh1P, TEXT("GripPoint"));
-	//FP_Gun->SetupAttachment(RootComponent);
-
-	//FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
-	//FP_MuzzleLocation->SetupAttachment(FP_Gun);
-	//FP_MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
-
-	// Default offset from the character location for projectiles to spawn
-	//GunOffset = FVector(100.0f, 0.0f, 10.0f);
-
-	// Note: The ProjectileClass and the skeletal mesh/anim blueprints for Mesh1P, FP_Gun, and VR_Gun 
-	// are set in the derived blueprint asset named MyCharacter to avoid direct content references in C++.
-
-	// Create a gun and attach it to the right-hand VR controller.
-	//// Create a gun mesh component
-	//VR_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("VR_Gun"));
-	//VR_Gun->SetOnlyOwnerSee(true);			// only the owning player will see this mesh
-	//VR_Gun->bCastDynamicShadow = false;
-	//VR_Gun->CastShadow = false;
-	//VR_Gun->SetupAttachment(R_MotionController);
-	//VR_Gun->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
-
-	//VR_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("VR_MuzzleLocation"));
-	//VR_MuzzleLocation->SetupAttachment(VR_Gun);
-	//VR_MuzzleLocation->SetRelativeLocation(FVector(0.000004, 53.999992, 10.000000));
-	//VR_MuzzleLocation->SetRelativeRotation(FRotator(0.0f, 90.0f, 0.0f));		// Counteract the rotation of the VR gun model.
-
-	// Uncomment the following line to turn motion controllers on by default:
-	//bUsingMotionControllers = true;
-
 	CurrentDoor = nullptr;
+	CurrentDocument = nullptr;
+	IsReadingDocument = false;
 }
 
 void ALiminalCharacter::BeginPlay()
@@ -85,25 +44,8 @@ void ALiminalCharacter::BeginPlay()
 	// Call the base class  
 	Super::BeginPlay();
 
-	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
-	//FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
-
-	// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
-	//if (bUsingMotionControllers)
-	//{
-	//	VR_Gun->SetHiddenInGame(false, true);
-	//	Mesh1P->SetHiddenInGame(true, true);
-	//}
-	//else
-	//{
-	//	VR_Gun->SetHiddenInGame(true, true);
-	//	Mesh1P->SetHiddenInGame(false, true);
-	//}
-
-	//if (InfoWidget)
-	//{
-	//	InfoWidget->AddToViewport();
-	//}
+	InfoWidget->AddToViewport();
+	InfoWidget->SetVisibility(ESlateVisibility::Hidden);
 }
 
 void ALiminalCharacter::Tick(float DeltaTime)
@@ -123,19 +65,25 @@ void ALiminalCharacter::Tick(float DeltaTime)
 	if (GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, CollisionParams))
 	{
 		if (Hit.bBlockingHit)
-		{	
-
+		{
+			InfoWidget->SetVisibility(ESlateVisibility::Visible);
 			if (Hit.GetActor()->GetClass()->IsChildOf(ADoor::StaticClass()))
 			{
 				//InfoWidget->GetWidgetFromName("helpimage")->SetVisibility(ESlateVisibility::Visible);
 				CurrentDoor = Cast<ADoor>(Hit.Actor);
+			}
+			else if (Hit.GetActor()->GetClass()->IsChildOf(APickupDocument::StaticClass()))
+			{
+				CurrentDocument = Cast<APickupDocument>(Hit.Actor);
 			}
 		}
 	}
 	else
 	{
 		//InfoWidget->GetWidgetFromName("helpimage")->SetVisibility(ESlateVisibility::Hidden);
+		InfoWidget->SetVisibility(ESlateVisibility::Hidden);
 		CurrentDoor = nullptr;
+		CurrentDocument = nullptr;
 	}
 }
 
@@ -151,9 +99,6 @@ void ALiminalCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerI
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	// Bind fire event
-	//PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &ALiminalCharacter::OnFire);
-
 	PlayerInputComponent->BindAction("Action", IE_Pressed, this, &ALiminalCharacter::OnAction);
 
 	// Bind movement events
@@ -163,63 +108,15 @@ void ALiminalCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerI
 	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
 	// "turn" handles devices that provide an absolute delta, such as a mouse.
 	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("Turn", this, &ALiminalCharacter::TurnAtRateMouse);
 	PlayerInputComponent->BindAxis("TurnRate", this, &ALiminalCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &ALiminalCharacter::LookUpAtRateMouse);
 	PlayerInputComponent->BindAxis("LookUpRate", this, &ALiminalCharacter::LookUpAtRate);
 }
-//
-//void ALiminalCharacter::OnFire()
-//{
-//	// try and fire a projectile
-//	if (ProjectileClass != NULL)
-//	{
-//		UWorld* const World = GetWorld();
-//		if (World != NULL)
-//		{
-//			if (bUsingMotionControllers)
-//			{
-//				const FRotator SpawnRotation = VR_MuzzleLocation->GetComponentRotation();
-//				const FVector SpawnLocation = VR_MuzzleLocation->GetComponentLocation();
-//				World->SpawnActor<ALiminalProjectile>(ProjectileClass, SpawnLocation, SpawnRotation);
-//			}
-//			else
-//			{
-//				const FRotator SpawnRotation = GetControlRotation();
-//				// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
-//				const FVector SpawnLocation = ((FP_MuzzleLocation != nullptr) ? FP_MuzzleLocation->GetComponentLocation() : GetActorLocation()) + SpawnRotation.RotateVector(GunOffset);
-//
-//				//Set Spawn Collision Handling Override
-//				FActorSpawnParameters ActorSpawnParams;
-//				ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
-//
-//				// spawn the projectile at the muzzle
-//				World->SpawnActor<ALiminalProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
-//			}
-//		}
-//	}
-//
-//	// try and play the sound if specified
-//	if (FireSound != NULL)
-//	{
-//		UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
-//	}
-//
-//	// try and play a firing animation if specified
-//	if (FireAnimation != NULL)
-//	{
-//		// Get the animation object for the arms mesh
-//		UAnimInstance* AnimInstance = Mesh1P->GetAnimInstance();
-//		if (AnimInstance != NULL)
-//		{
-//			AnimInstance->Montage_Play(FireAnimation, 1.f);
-//		}
-//	}
-//}
 
 void ALiminalCharacter::MoveForward(float Value)
 {
-	if (Value != 0.0f)
+	if (Value != 0.0f && !IsReadingDocument)
 	{
 		// add movement in that direction
 		AddMovementInput(GetActorForwardVector(), Value);
@@ -228,7 +125,7 @@ void ALiminalCharacter::MoveForward(float Value)
 
 void ALiminalCharacter::MoveRight(float Value)
 {
-	if (Value != 0.0f)
+	if (Value != 0.0f && !IsReadingDocument)
 	{
 		// add movement in that direction
 		AddMovementInput(GetActorRightVector(), Value);
@@ -237,14 +134,38 @@ void ALiminalCharacter::MoveRight(float Value)
 
 void ALiminalCharacter::TurnAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+	if (!IsReadingDocument)
+	{
+		// calculate delta for this frame from the rate information
+		AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+	}
+}
+
+void ALiminalCharacter::TurnAtRateMouse(float Rate)
+{
+	if (!IsReadingDocument)
+	{
+		// calculate delta for this frame from the rate information
+		AddControllerYawInput(Rate);
+	}
 }
 
 void ALiminalCharacter::LookUpAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+	if (!IsReadingDocument)
+	{
+		// calculate delta for this frame from the rate information
+		AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+	}
+}
+
+void ALiminalCharacter::LookUpAtRateMouse(float Rate)
+{
+	if (!IsReadingDocument)
+	{
+		// calculate delta for this frame from the rate information
+		AddControllerPitchInput(Rate);
+	}
 }
 
 void ALiminalCharacter::OnAction()
@@ -252,5 +173,17 @@ void ALiminalCharacter::OnAction()
 	if (CurrentDoor)
 	{
 		CurrentDoor->ToggleDoor();
+	}
+	else if (CurrentDocument && !IsReadingDocument)
+	{
+		IsReadingDocument = true;
+		DocumentWidget = CurrentDocument->GetDocumentContent();
+		DocumentWidget->AddToViewport();
+		CurrentDocument->Destroy();
+	}
+	else if (IsReadingDocument)
+	{
+		IsReadingDocument = false;
+		DocumentWidget->RemoveFromViewport();
 	}
 }
